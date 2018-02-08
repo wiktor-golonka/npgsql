39ea1d7ec3ca3ffcc1062fc5a79cb1807db36030
Shay Rojansky <roji@roji.org>
2018-02-07 18:59:46 -0800

    Cache StateChangeEventArgs on connection
    Avoid allocating StateChangeEventArgs on every open/close by caching
    them.
    
    See https://github.com/dotnet/corefx/issues/14934

--- src/Npgsql/NpgsqlConnection.cs
+++ src/Npgsql/NpgsqlConnection.cs
@@ -122,6 +122,9 @@ namespace Npgsql
 
         static bool _countersInitialized;
 
+        static readonly StateChangeEventArgs ClosedToOpenEventArgs = new StateChangeEventArgs(ConnectionState.Closed, ConnectionState.Open);
+        static readonly StateChangeEventArgs OpenToClosedEventArgs = new StateChangeEventArgs(ConnectionState.Open, ConnectionState.Closed);
+
         #endregion Fields
 
         #region Constructors / Init / Open
@@ -275,7 +278,7 @@ namespace Npgsql
                 throw;
             }
             Log.Debug("Connection opened", Connector.Id);
-            OnStateChange(new StateChangeEventArgs(ConnectionState.Closed, ConnectionState.Open));
+            OnStateChange(ClosedToOpenEventArgs);
         }
 
         #endregion Open / Init
@@ -597,7 +600,7 @@ namespace Npgsql
 
             Connector = null;
 
-            OnStateChange(new StateChangeEventArgs(ConnectionState.Open, ConnectionState.Closed));
+            OnStateChange(OpenToClosedEventArgs);
         }
 
         /// <summary>
